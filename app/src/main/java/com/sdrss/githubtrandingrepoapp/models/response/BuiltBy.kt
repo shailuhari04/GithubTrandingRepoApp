4db1951626bc48538520b21bac42a6ba4a40b097
fitShailu <57788604+fitshailendra@users.noreply.github.com>
2020-01-23 03:35:10 +0530

    ResponseModel : Data Class created for remote response. Git : update gitignore file

---
+++ app/src/main/java/com/sdrss/githubtrandingrepoapp/models/response/BuiltBy.kt
@@ -0,0 +1,11 @@
+package com.sdrss.githubtrandingrepoapp.models.response
+
+
+import androidx.annotation.Keep
+
+@Keep
+data class BuiltBy(
+    val avatar: String = "", // https://avatars0.githubusercontent.com/u/376065
+    val href: String = "", // https://github.com/viatsko
+    val username: String = "" // viatsko
+)
