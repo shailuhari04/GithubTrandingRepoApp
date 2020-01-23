3773b5be2898d63c5bfff9bbfac8f566969125cf
fitShailu <57788604+fitshailendra@users.noreply.github.com>
2020-01-23 16:10:57 +0530

    Extension : Check Internet Connectivity Git : update gitignore file

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,115 +1,119 @@
 object Versions {
-    val retrofit = "2.3.0"
-    val source_compat = "1.8"
-    val target_compat = "1.8"
-    val min_sdk = 21
-    val target_sdk = 28
-    val compile_sdk = 28
-    val build_tools = "28.0.3"
+    const val RETROFIT = "2.3.0"
+    const val SOURCE_COMPAT = "1.8"
+    const val TARGET_COMPAT = "1.8"
+    const val MIN_SDK = 21
+    const val TARGET_SDK = 28
+    const val COMPILE_SDK = 28
+    const val BUILD_TOOLS = "28.0.3"
 
     // Kotlin
-    val kotlin = "1.3.61"
-    val kotlin_coroutines = "1.2.2"
-    val apache_commons = "2.4"
-    val retrofit_coroutines_adapter = "0.9.2"
+    const val KOTLIN = "1.3.61"
+    const val KOTLIN_COROUTINES = "1.2.2"
+    const val APACHE_COMMONS = "2.4"
+    const val RETROFIT_COROUTINES_ADAPTER = "0.9.2"
 
-    val stetho = "1.5.0"
-    val okhttp = "3.10.0"
-    val glide = "4.9.0"
-    val process_phoenix = "2.0.0"
-    val chuck = "1.1.0"
-    val junit = "4.12"
-    val junit_ext = "1.1.0"
-    val wiremock = "2.18.0"
-    val timber = "4.5.1"
-    val moshi = "1.6.0"
+    const val STETHO = "1.5.0"
+    const val OKHTTP = "3.10.0"
+    const val GLIDE = "4.9.0"
+    const val JUNIT = "4.12"
+    const val JUNIT_EXT = "1.1.0"
+    const val WIRERE_MOKE = "2.18.0"
+    const val TIMBER = "4.5.1"
+    const val MOSHI = "1.6.0"
 
     // android
-    val androidx = "1.0.0"
-    val androidx_lifecycle = "2.0.0"
-    val androidx_persistence = "2.2.0-alpha01"
-    val androidx_test = "1.1.0"
-    val androidx_constraintlayout = "1.1.3"
+    const val ANDROIDX = "1.0.0"
+    const val ANDROIDX_LIFECYCLE = "2.0.0"
+    const val ANDROIDX_PERSISTENCE = "2.2.0-alpha01"
+    const val ANDROIDX_TEST = "1.1.0"
+    const val ANDROIDX_CONSTRAINTLAYOUT = "1.1.3"
 
-    val android_gradle_plugin = "3.5.3"
-    val espresso = "3.1.1"
-    val test_support_lib = "1.0.1"
+    const val ANDROID_GRADLE_PLUGIN = "3.5.3"
+    const val ESPRESSO = "3.1.1"
+    const val TEST_SUPPORT_LIB = "1.0.1"
 
-    val assertj = "3.11.1"
-    val android_debug_database = "1.0.6"
+    const val ASSERTJ = "3.11.1"
+    const val ANDROID_DEBUG_DATABASE = "1.0.6"
 }
 
 object Libs {
-    val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
-    val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
-    val retrofit_moshi_converter = "com.squareup.retrofit2:converter-moshi:${Versions.retrofit}"
-    val process_phoenix = "com.jakewharton:process-phoenix:${Versions.process_phoenix}"
+    const val RETROFIT = "com.squareup.retrofit2:retrofit:${Versions.RETROFIT}"
+    const val GLIDE = "com.github.bumptech.glide:glide:${Versions.GLIDE}"
+    const val RETROFIT_MOSHI_CONVERTER =
+        "com.squareup.retrofit2:converter-moshi:${Versions.RETROFIT}"
 
     //AndroidX
-    val androidx_material = "com.google.android.material:material:${Versions.androidx}"
-    val androidx_recyclerview = "androidx.recyclerview:recyclerview:${Versions.androidx}"
-    val androidx_appcompat_v7 = "androidx.appcompat:appcompat:${Versions.androidx}"
-    val androidx_support_v4 = "androidx.legacy:legacy-support-v4:${Versions.androidx}"
-    val androidx_annotations = "androidx.annotation:annotation:${Versions.androidx}"
-    val androidx_constraintlayout = "androidx.constraintlayout:constraintlayout:${Versions.androidx_constraintlayout}"
+    const val ANDROIDX_MATERIAL = "com.google.android.material:material:${Versions.ANDROIDX}"
+    const val ANDROIDX_RECYCLERVIEW = "androidx.recyclerview:recyclerview:${Versions.ANDROIDX}"
+    const val ANDROIDX_APPCOMPAT_V7 = "androidx.appcompat:appcompat:${Versions.ANDROIDX}"
+    const val ANDROIDX_SUPPORT_V4 = "androidx.legacy:legacy-support-v4:${Versions.ANDROIDX}"
+    const val androidx_annotations = "androidx.annotation:annotation:${Versions.ANDROIDX}"
+    const val ANDROIDX_CONSTRAINTLAYOUT =
+        "androidx.constraintlayout:constraintlayout:${Versions.ANDROIDX_CONSTRAINTLAYOUT}"
 
 
-    val androidx_persistence_room_runtime = "androidx.room:room-runtime:${Versions.androidx_persistence}" // Room
-    val androidx_persistence_room_kapt = "androidx.room:room-compiler:${Versions.androidx_persistence}" // Annotation Processor
+    const val ANDROIDX_PERSISTENCE_ROOM_RUNTIME =
+        "androidx.room:room-runtime:${Versions.ANDROIDX_PERSISTENCE}" // Room
+    const val ANDROIDX_PERSISTENCE_ROOM_KAPT =
+        "androidx.room:room-compiler:${Versions.ANDROIDX_PERSISTENCE}" // Annotation Processor
 
-    val androidx_lifecycle_livedata_core = "androidx.lifecycle:lifecycle-livedata-core:${Versions.androidx_lifecycle}"
+    const val ANDROIDX_LIFECYCLE_LIVEDATA_CORE =
+        "androidx.lifecycle:lifecycle-livedata-core:${Versions.ANDROIDX_LIFECYCLE}"
 
-    val androidx_persistence_room_ktx = "androidx.room:room-ktx:${Versions.androidx_persistence}" // optional - Kotlin Extensions and Coroutines support for Room
-    val androidx_persistence_room_test = "androidx.room:room-testing:${Versions.androidx_persistence}" // Test helpers
+    const val ANDROIDX_PERSISTENCE_ROOM_KTX =
+        "androidx.room:room-ktx:${Versions.ANDROIDX_PERSISTENCE}" // optional - Kotlin Extensions and Coroutines support for Room
 
+    const val ANDROIDX_PERSISTENCE_ROOM_TEST =
+        "androidx.room:room-testing:${Versions.ANDROIDX_PERSISTENCE}" // Test helpers
+    const val ANDROIDX_TEST_RUNNER = "androidx.test:runner:${Versions.ANDROIDX_TEST}"
+    const val ANDROIDX_TEST_RULES = "androidx.test:rules:${Versions.ANDROIDX_TEST}"
 
-    val androidx_testrunner = "androidx.test:runner:${Versions.androidx_test}"
-    val androidx_test_rules = "androidx.test:rules:${Versions.androidx_test}"
+    const val OKHTTP = "com.squareup.okhttp3:okhttp:${Versions.OKHTTP}"
+    const val OKHTTP_LOGGING_INTERCEPTOR =
+        "com.squareup.okhttp3:logging-interceptor:${Versions.OKHTTP}"
+    const val OKHTTP_MOCKWEBSERVER = "com.squareup.okhttp3:mockwebserver:${Versions.OKHTTP}"
+    const val RETROFIT_CONVERTER_GSON = "com.squareup.retrofit2:converter-gson:${Versions.RETROFIT}"
+    const val MOSHI = "com.squareup.moshi:moshi:${Versions.MOSHI}"
+    const val MOSHI_KOTLIN = "com.squareup.moshi:moshi-kotlin:${Versions.MOSHI}"
+    const val WIRERE_MOKE = "com.github.tomakehurst:wiremock:${Versions.WIRERE_MOKE}"
+    const val KOTLIN_STD_LIB = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.KOTLIN}"
+    const val KOTLIN_COROUTINES =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.KOTLIN_COROUTINES}"
+    const val KOTLIN_COROUTINES_ANDROID =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.KOTLIN_COROUTINES}"
+    const val RETROFIT_COROUTINES_ADAPTER =
+        "com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:${Versions.RETROFIT_COROUTINES_ADAPTER}"
+    const val KOTLIN_REFLECT = "org.jetbrains.kotlin:kotlin-reflect:${Versions.KOTLIN}"
+    const val COROUTINES =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.KOTLIN_COROUTINES}"
+    const val COROUTINES_ANDROID =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.KOTLIN_COROUTINES}"
+    const val APACHE_HTTP_CLIENT_ANDROID = "org.apache.httpcomponents:httpclient-android:4.3.5.1"
 
-    val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttp}"
-    val okhttp_logging_interceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp}"
-    val okhttp_mockwebserver = "com.squareup.okhttp3:mockwebserver:${Versions.okhttp}"
-    val retrofit_converter_gson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
-    val moshi = "com.squareup.moshi:moshi:${Versions.moshi}"
-    val moshiKotlin = "com.squareup.moshi:moshi-kotlin:${Versions.moshi}"
-    val wiremock = "com.github.tomakehurst:wiremock:${Versions.wiremock}"
-    val apacheHttpClientAndroid = "org.apache.httpcomponents:httpclient-android:4.3.5.1"
-    val kotlin_std_lib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
-    val kotlin_coroutines =
-        "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.kotlin_coroutines}"
-    val kotlin_coroutines_android =
-        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.kotlin_coroutines}"
-    val retrofit_coroutines_adapter =
-        "com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:${Versions.retrofit_coroutines_adapter}"
-    val kotlin_reflect = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
-    val coroutines =
-        "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.kotlin_coroutines}"
-    val coroutines_android =
-        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.kotlin_coroutines}"
-
-    val timber = "com.jakewharton.timber:timber:${Versions.timber}"
-    val stetho = "com.facebook.stetho:stetho:${Versions.stetho}"
-    val stetho_okhttp3 = "com.facebook.stetho:stetho-okhttp3:${Versions.stetho}"
-    val chuck = "com.readystatesoftware.chuck:library:${Versions.chuck}"
-    val chuck_no_op = "com.readystatesoftware.chuck:library-no-op:${Versions.chuck}"
-    val apache_commons = "commons-io:commons-io:${Versions.apache_commons}"
-    val android_debug_database = "com.amitshekhar.android:debug-db:${Versions.android_debug_database}"
+    const val TIMBER = "com.jakewharton.timber:timber:${Versions.TIMBER}"
+    const val STETHO = "com.facebook.stetho:stetho:${Versions.STETHO}"
+    const val STETHO_OKHTTP3 = "com.facebook.stetho:stetho-okhttp3:${Versions.STETHO}"
+    const val APACHE_COMMONS = "commons-io:commons-io:${Versions.APACHE_COMMONS}"
+    const val ANDROID_DEBUG_DATABASE =
+        "com.amitshekhar.android:debug-db:${Versions.ANDROID_DEBUG_DATABASE}"
 
 }
 
 object TestLibs {
-    val runner = "com.android.support.test:runner:${Versions.test_support_lib}"
-    val orchestrator = "com.android.support.test:orchestrator:${Versions.test_support_lib}"
-    val espresso_core = "com.android.support.test.espresso:espresso-core:${Versions.espresso}"
-    val espresso_contrib = "com.android.support.test.espresso:espresso-contrib:${Versions.espresso}"
-    val android_test_runner = "com.android.support.test:runner:${Versions.test_support_lib}"
-    val testRules = "com.android.support.test:rules:${Versions.test_support_lib}"
-    val junit = "junit:junit:${Versions.junit}"
-    val junit_ext = "androidx.test.ext:junit:${Versions.junit_ext}"
-    val assertj = "org.assertj:assertj-core:${Versions.assertj}"
+    const val RUNNER = "com.android.support.test:runner:${Versions.TEST_SUPPORT_LIB}"
+    const val ORCHESTRATOR = "com.android.support.test:orchestrator:${Versions.TEST_SUPPORT_LIB}"
+    const val ESPRESSO_CORE = "com.android.support.test.espresso:espresso-core:${Versions.ESPRESSO}"
+    const val ESPRESSO_CONTRIB =
+        "com.android.support.test.espresso:espresso-contrib:${Versions.ESPRESSO}"
+    const val ANDROID_TEST_RUNNER = "com.android.support.test:runner:${Versions.TEST_SUPPORT_LIB}"
+    const val TEST_RULES = "com.android.support.test:rules:${Versions.TEST_SUPPORT_LIB}"
+    const val JUNIT = "junit:junit:${Versions.JUNIT}"
+    const val JUNIT_EXT = "androidx.test.ext:junit:${Versions.JUNIT_EXT}"
+    const val ASSERTJ = "org.assertj:assertj-core:${Versions.ASSERTJ}"
 }
 
 object GradlePlugins {
-    val android = "com.android.tools.build:gradle:${Versions.android_gradle_plugin}"
-    val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val ANDROID = "com.android.tools.build:gradle:${Versions.ANDROID_GRADLE_PLUGIN}"
+    const val KOTLIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN}"
 }
